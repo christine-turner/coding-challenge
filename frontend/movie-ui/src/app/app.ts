dbce2da60d97fd280b1b0dabc0059a75dbc6be85
Chrissy Turner <christine.hill@gmail.com>
2025-10-25 17:28:21 +1030

    cors working

--- frontend/movie-ui/src/app/app.ts
+++ frontend/movie-ui/src/app/app.ts
@@ -1,19 +1,29 @@
 import { Component } from '@angular/core';
 import { MovieTableComponent, Movie } from './movie-table.component';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [MovieTableComponent],
-  template: `
-    <h1>Movie List</h1>
-    <app-movie-table [movies]="movies"></app-movie-table>
-  `,
+  imports: [CommonModule, MovieTableComponent], 
+  templateUrl: './app.component.html'
 })
 export class App {
-  movies: Movie[] = [
+ /* movies: Movie[] = [
     { title: 'Inception', year: 2010, genres: ['Action', 'Sci-Fi'] },
     { title: 'The Matrix', year: 1999, genres: ['Action', 'Sci-Fi'] },
-    { title: 'Interstellar', year: 2014, genres: ['Adventure', 'Drama', 'Sci-Fi'] },
-  ];
+    { title: 'Interstellar', year: 2014, genres: ['Adventure', 'Drama'] }
+  ]; */
+
+    movies: Movie[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get<Movie[]>('http://localhost:8080/api/movies')
+      .subscribe(data => {
+        this.movies = data;
+      });
+  }
 }
--- frontend/movie-ui/src/app/movie-table.component.ts
+++ frontend/movie-ui/src/app/movie-table.component.ts
@@ -1,12 +1,10 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Input, ViewChild,  } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { CommonModule } from '@angular/common';
-import { MatTableModule } from '@angular/material/table';
 import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
 import { MatSort, MatSortModule } from '@angular/material/sort';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatTableDataSource } from '@angular/material/table';
 
+// Define an interface for your movie data
 export interface Movie {
   title: string;
   year: number;
@@ -16,72 +14,27 @@ export interface Movie {
 @Component({
   selector: 'app-movie-table',
   standalone: true,
-  imports: [
-    CommonModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatFormFieldModule,
-    MatInputModule,
-  ],
-  template: `
-    <div>
-      <mat-form-field appearance="outline">
-        <mat-label>Filter</mat-label>
-        <input matInput (keyup)="applyFilter($event)" placeholder="Filter movies" />
-      </mat-form-field>
-
-      <table mat-table [dataSource]="dataSource" matSort class="mat-elevation-z8">
-        
-        <!-- Title Column -->
-        <ng-container matColumnDef="title">
-          <th mat-header-cell *matHeaderCellDef mat-sort-header> Title </th>
-          <td mat-cell *matCellDef="let movie"> {{ movie.title }} </td>
-        </ng-container>
-
-        <!-- Year Column -->
-        <ng-container matColumnDef="year">
-          <th mat-header-cell *matHeaderCellDef mat-sort-header> Year </th>
-          <td mat-cell *matCellDef="let movie"> {{ movie.year }} </td>
-        </ng-container>
-
-        <!-- Genres Column -->
-        <ng-container matColumnDef="genres">
-          <th mat-header-cell *matHeaderCellDef> Genres </th>
-          <td mat-cell *matCellDef="let movie"> {{ movie.genres.join(', ') }} </td>
-        </ng-container>
-
-        <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
-        <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
-      </table>
-
-      <mat-paginator
-        [pageSizeOptions]="[5, 10, 25]"
-        showFirstLastButtons>
-      </mat-paginator>
-    </div>
-  `,
+  imports: [CommonModule, MatTableModule, MatPaginatorModule, MatSortModule],
+  templateUrl: './movie-table.component.html',
+  styleUrls: ['./movie-table.component.scss']
 })
-export class MovieTableComponent implements OnInit {
+
+export class MovieTableComponent implements AfterViewInit {
   @Input() movies: Movie[] = [];
 
-  displayedColumns: string[] = ['title', 'year', 'genres'];
-  dataSource = new MatTableDataSource<Movie>([]);
+  displayedColumns: string[] = ['title', 'year', 'genres']; 
+  
+  dataSource = new MatTableDataSource<Movie>();
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
-  ngOnInit() {
-    this.dataSource.data = this.movies;
-  }
-
   ngAfterViewInit() {
+    this.dataSource.data = this.movies;
     this.dataSource.paginator = this.paginator;
     this.dataSource.sort = this.sort;
   }
-
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value.trim().toLowerCase();
-    this.dataSource.filter = filterValue;
-  }
+  ngOnChanges() {
+  this.dataSource.data = this.movies;
+}
 }
